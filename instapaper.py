be7ab3eded7f8af4b9c4d1567bc9b87febd3350f
Oleg G PC <git@golubevog.ru>
2017-01-08 02:32:09 +0300

    add url to instapaper test

---
+++ instapaper.py
@@ -0,0 +1,45 @@
+"""Small library of functions using Instapaper's simple API.
+https://www.instapaper.com/api/simple
+"""
+import requests
+
+def check_request_status(response_code):
+    responses = {'200': 'Login successful!',
+               '201': 'Article saved!',
+               '400': 'Bad request or exceeded the rate limit. Probably missing a required parameter, such as url',
+               '401': 'Malformed request. Please try again.',
+               '403': 'Username or password incorrect. Please try again.',
+               '500': 'The service encountered an error. Please try again later'}
+    try:
+        print (responses[response_code])
+    except KeyError:
+        print ('Something went wrong:', response_code)
+
+
+def authenticate(username, password):
+    auth_url = 'https://www.instapaper.com/api/authenticate'
+    authenticated = False
+
+    while not authenticated:
+        auth_payload = {'username': username,
+                    'password': password}
+        auth_request = requests.get(auth_url, params=auth_payload)
+        check_request_status(str(auth_request.status_code))
+        if auth_request.text == '200':
+            authenticated = True
+            return authenticated
+        else:
+            username = input('please, retype your LOGIN: ')
+            password = input('please, retype your PASSWORD: ')
+
+
+def add_urls(username,password,url):
+    if authenticate(username,password):
+        add_payload = {'username': username,
+                       'password': password,
+                       'url': url}
+        add_url = 'https://www.instapaper.com/api/add'
+        add_request = requests.get(add_url, params=add_payload)
+        check_request_status (str(add_request.status_code))
+    else:
+        print('please LOGIN')
--- instapaperbot.py
+++ instapaperbot.py
@@ -3,6 +3,8 @@ import re
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+import instapaper
+import config #временно загружаю логин-пароль ль instapaper
 
 def log_message(log_info):
     user_id = log_info['message']['chat']['id']
@@ -16,29 +18,30 @@ def start (bot, update):
     bot.sendMessage(chat_id = update.message.chat_id, text = 'Hello')
     print ('yes')
 
+def add_url_to_instapaper(chat_id,url):
+    username = config.user
+    password = config.password
+    if str(chat_id) == config.id:
+        instapaper.add_urls(username,password,url)
+    else:
+        print ("can't add link:{} from id:{}".format(url,chat_id))
+
 def find_url(text):
     pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
     res = pattern.findall(text)
     return res
-
 
 def conversation (bot, update):
     message_text = update['message']['text']
     clear_url = find_url(message_text)
     if len(clear_url) != 0:
         print ('url=',clear_url)
+        for single_url in clear_url:
+            add_url_to_instapaper(update.message.chat_id,single_url)
         bot.sendMessage(chat_id = update.message.chat_id,text = 'Thanks for the link, I soon learned to handle it')
     else:
         bot.sendMessage(chat_id = update.message.chat_id,text = update.message.text)
     log_message(update)
-
-def forward_to_instapapper(bot,update):
-    print ('forward message')
-    message_text = update['message']['text']
-    clear_url = find_url(message_text)
-    print (clear_url)
-    log_message(update)
-
 
 def unknown(bot,update):
     bot.sendMessage(chat_id = update.message.chat_id,text = "I didn't understand that command.")
@@ -51,17 +54,15 @@ def main ():
     logging.basicConfig(filename='info.log',level = logging.INFO,format='%(asctime)s - %(message)s')
     updater = Updater (token = TOKEN)
     dispatcher = updater.dispatcher
+    #handlers
     start_handler = CommandHandler('start', start)
     conversation_handler = MessageHandler (Filters.text, conversation)
-    forward_handler = MessageHandler(Filters.forwarded,forward_to_instapapper)
     unknown_handler = MessageHandler(Filters.command, unknown)
-
+    #dispatchers
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(conversation_handler)
     dispatcher.add_handler(unknown_handler)
-    dispatcher.add_handler(forward_handler)
-    dispatcher.add_handler(unknown_handler)
-
+    #updater
     updater.start_polling()
 
 
