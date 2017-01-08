b2b4d0b49e755da0fc56e83ce81312d148f315d6
Oleg G PC <git@golubevog.ru>
2017-01-08 22:49:33 +0300

    add comments to code

--- instapaperbot.py
+++ instapaperbot.py
@@ -6,18 +6,21 @@ from telegram.ext import MessageHandler, Filters
 import instapaper #add link to instapaper
 import config #временно загружаю логин-пароль ль instapaper
 
+#логирование всех данных
 def log_message(log_info):
     user_id = log_info['message']['chat']['id']
     user_username = log_info['message']['chat']['username']
     user_text = log_info['message']['text']
     debug_info = 'id:{} user:{} text:"{}"'.format(user_id,user_username,user_text)
-    #print (debug_info)
     logging.info(debug_info)
 
+#реакция при нажатии команды Start
 def start (bot, update):
     bot.sendMessage(chat_id = update.message.chat_id, text = 'Hello, new user!')
     print ('New user')
 
+#добавлении адреса в Instapaper конкретного человека
+#пока можно установить собственный логин-пароль и постить туда адреса
 def add_url_to_instapaper(chat_id,url):
     username = config.user
     password = config.password
@@ -26,11 +29,13 @@ def add_url_to_instapaper(chat_id,url):
     else:
         print ("can't add link:{} from id:{}".format(url,chat_id))
 
+#регулярное выражение для поиска URL в сообщении
 def find_url(text):
     pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]|[А-Яа-я]))+')
     res = pattern.findall(text)
     return res
 
+#основная функция по общению с пользователем
 def conversation (bot, update):
     message_text = update['message']['text']
     clear_url = find_url(message_text)
@@ -43,9 +48,11 @@ def conversation (bot, update):
         bot.sendMessage(chat_id = update.message.chat_id,text = update.message.text)
     log_message(update)
 
+#реакция на неизвестные команды
 def unknown(bot,update):
     bot.sendMessage(chat_id = update.message.chat_id,text = "I didn't understand that command.")
 
+#основная функция программы
 def main ():
     TOKEN = config.TOKEN
     #простое логгирование в файл
@@ -62,7 +69,6 @@ def main ():
     dispatcher.add_handler(unknown_handler)
     #updater
     updater.start_polling()
-
 
 
 if __name__ == '__main__':
